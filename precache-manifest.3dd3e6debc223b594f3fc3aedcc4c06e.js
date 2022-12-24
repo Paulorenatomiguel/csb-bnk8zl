1295de473133813a4bd4a8b2872c3b2abaf5316e
Code Sandbox <hello@codesandbox.io>
2022-12-24 19:12:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3dd3e6debc223b594f3fc3aedcc4c06e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a641b20ab87395395303",
+    "url": "https://paulorenatomiguel.github.io/csb-bnk8zl/static/css/main.6691d762.chunk.css"
+  },
+  {
+    "revision": "a641b20ab87395395303",
+    "url": "https://paulorenatomiguel.github.io/csb-bnk8zl/static/js/main.a641b20a.chunk.js"
+  },
+  {
+    "revision": "d3158d9747f7637e38c9",
+    "url": "https://paulorenatomiguel.github.io/csb-bnk8zl/static/js/1.d3158d97.chunk.js"
+  },
+  {
+    "revision": "e9b2c2bda6a539e654e8",
+    "url": "https://paulorenatomiguel.github.io/csb-bnk8zl/static/js/runtime~main.e9b2c2bd.js"
+  },
+  {
+    "revision": "71ccd1448072528987a24b0ee9337fe9",
+    "url": "https://paulorenatomiguel.github.io/csb-bnk8zl/index.html"
+  }
+];
